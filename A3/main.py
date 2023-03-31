fe2823516420e2e65cbbf630f8fc2db662cd5ab8
agSidharth <sidharthagarwal2001@gmail.com>
2023-03-31 12:51:22 +0530

    Q1 completed

--- A3/main.py
+++ A3/main.py
@@ -7,25 +7,18 @@ import scipy
 import math
 
 NORMALIZE = False
-TEST = True
 
 class Calibrator:
     def __init__(self,fileName):
+        self.w,self.p = self.retCordinates(fileName)
+        self.n = self.w.shape[0]
+        self.calibrate()
 
+    def retCordinates(self,fileName):
         df = pd.read_csv(fileName)
-        self.n = len(df.index)
-
         threeD_cols = df.columns[0:3]
         twoD_cols = df.columns[3:5]
-        self.w = df[threeD_cols].to_numpy()
-        self.p = df[twoD_cols].to_numpy()
-
-        if TEST:
-            perm = np.random.permutation(range(self.n))
-            self.p_test = np.append(self.p[perm],np.ones((self.n,1)),axis=1)
-            self.w_test = np.append(self.w[perm],np.ones((self.n,1)),axis=1)
-
-        self.calibrate()
+        return df[threeD_cols].to_numpy(), df[twoD_cols].to_numpy()
 
     # see if this needs to be done or not...
     def normalise(self,D):
@@ -66,6 +59,13 @@ class Calibrator:
         self.K = self.K/self.K[2,2]
         self.K = self.K*np.diag(np.sign(self.K))
 
+        self.fx = self.K[0,0]
+        self.s = self.K[0,1]
+        self.cx = self.K[0,2]
+        self.fy = self.K[1,1]
+        self.cy = self.K[1,2]
+
+        """
         self.alpha = self.K[0,0]
         tanTheta = -self.alpha/self.K[0,1]
         self.theta = math.atan(tanTheta)
@@ -75,17 +75,26 @@ class Calibrator:
         self.y0 = self.K[1,2]
         self.beta = self.K[1,1]*math.sin(self.theta)
         self.theta = math.degrees(self.theta)
+        """
         pass
 
     def printparams(self):
         print("The K matrix is as follows : ")
         print(self.K)
         print("The intrinsic parameters obtained are as follows : ")
+        print("fx : "+str(self.fx))
+        print("fy : "+str(self.fy))
+        print("s : "+str(self.s))
+        print("cx : "+str(self.cx))
+        print("cy : "+str(self.cy))
+
+        """
         print("Alpha : "+str(self.alpha))
         print("Beta : "+str(self.beta))
         print("Theta : "+str(self.theta)+" degrees")
         print("x0 : "+str(self.x0))
         print("y0 : "+str(self.y0))
+        """
 
     def calibrate(self):
 
@@ -101,13 +110,19 @@ class Calibrator:
         self.extract_params()
         self.printparams()
     
-    def test(self):
+    def test(self,fileName):
+        self.w_test,self.p_test = self.retCordinates(fileName)
+        self.nt = self.w_test.shape[0]
+
+        self.p_test = np.append(self.p_test,np.ones((self.nt,1)),axis=1)
+        self.w_test = np.append(self.w_test,np.ones((self.nt,1)),axis=1)
+
         pts = np.matmul(self.M,self.w_test.T).T
         pts[:,0] = pts[:,0]/pts[:,2]
         pts[:,1] = pts[:,1]/pts[:,2]
-        pts[:,2] = np.ones((self.n))
+        pts[:,2] = np.ones((self.nt))
 
-        rmse = np.linalg.norm(pts-self.p_test)/np.sqrt(self.n)
+        rmse = np.linalg.norm(pts-self.p_test)/np.sqrt(self.nt)
         print("The rmse error is : "+str(rmse))
 
         plt.scatter(self.p_test[:,0],self.p_test[:,1],marker = 'x')
@@ -122,7 +137,7 @@ class Calibrator:
 
 if __name__=='__main__':
     ourCalib = Calibrator(sys.argv[1])
-    if TEST: ourCalib.test()
+    if len(sys.argv)>2: ourCalib.test(sys.argv[2])
     
 
     
